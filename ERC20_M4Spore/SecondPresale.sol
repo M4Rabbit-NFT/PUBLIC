1847ceaaf9cff6e6fe980621873a41e3eaeca0d6
MacGuyver <89907172+macguyversmusic@users.noreply.github.com>
2022-05-28 21:06:13 -0500

    Update SecondPresale.sol

--- ERC20_M4Spore/SecondPresale.sol
+++ ERC20_M4Spore/SecondPresale.sol
@@ -1,28 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-
+pragma solidity ^0.8.14;
 // Imports
-import "./Libraries.sol";
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ */
+abstract contract ReentrancyGuard {
 
-contract SecondPresale is ReentrancyGuard {
-    address public owner; // owner multisig
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+        _status = _NOT_ENTERED;
+    }
+}
+
+contract pubSale is ReentrancyGuard {
+    address public owner = 0xBEED5427b0E728AC7EfAaD279c51d511472f9ee2; // owner
     IERC20 public token; //  Token.
     bool private tokenAvailable = false;
-    uint public tokensPerETH = 5000; // new quantity of tokens per bnb half as many as presale 1
-    uint public ending; // end time
-    bool public presaleStarted = false; // true or false
-    address public deadWallet = 0x0000000000000000000000000000000000000000; // burn Wallet.
-    uint public cooldownTime = 0 days; //time between withdrawals to prevent whales
-    uint public tokensSold;//sold quantity
+    uint public tokensPerETH = 17500; // token per ETH
+    uint public ending; // sale end time
+    bool public presaleStarted = false; //started or not
+    address public deadWallet = 0x000000000000000000000000000000000000dEaD; 
+    uint public cooldownTime = 7 days; // time between withdrawals of token
+    uint public tokensSold;
+    uint256 internal balance;
+    uint256 public ContractBalance = 1500000*10**18; 
 
-    mapping(address => bool) public whitelist; // Whitelist of investors for presale
-    mapping(address => uint) public invested; //how much bnb have they invested
-    mapping(address => uint) public investorBalance;//what is their balance
-    mapping(address => uint) public withdrawableBalance;//how much of that can they withdraw
-    mapping(address => uint) public claimReady;//is the claim ready
 
-    constructor(address _teamWallet) {
-        owner = _teamWallet;
+    mapping(address => uint) public invested; // how much a person invested.
+    mapping(address => uint) public investorBalance;//their current balance
+    mapping(address => uint) public withdrawableBalance;//how much they can take out of tha platform
+    mapping(address => uint) public claimReady;//is it time for that to happen
+
+    constructor() {
     }
 
     modifier onlyOwner() {
@@ -30,43 +50,47 @@ contract SecondPresale is ReentrancyGuard {
         _;
     }
 
-    //set the token can do only 1 time
+    function transferOwnership (address newOwner) public onlyOwner{
+        //check if not empty
+        if (newOwner != 0x0000000000000000000000000000000000000000){
+        owner = newOwner;
+        }
+    }
+
+   //token insertion can only happen 1 time
     function setToken(IERC20 _token) public onlyOwner {
         require(!tokenAvailable, "Token is already inserted.");
         token = _token;
         tokenAvailable = true;
     }
 
-    //add to whitelist
-    function addToWhitelist(address _investor) public onlyOwner {
-            require(_investor != address(0), 'Invalid address.');
-            address _investorAddress = _investor;
-            whitelist[_investorAddress] = true;        
+    function setPrice(uint _priceTPETH) public onlyOwner {
+        require(presaleStarted, "Presale not started.");
+        require(block.timestamp <  ending, "Presale finished.");
+        tokensPerETH = _priceTPETH;
     }
 
-    function startPresale(uint _presaleTime) public onlyOwner {
+    function startSale(uint _presaleTime) public onlyOwner {
+        require(tokenAvailable, "Token is not set.");
         require(!presaleStarted, "Presale already started.");
-
         ending = block.timestamp + _presaleTime;
         presaleStarted = true;
     }
 
-    //buy token
     function invest() public payable nonReentrant {
-        require(whitelist[msg.sender], "You must be on the whitelist.");
         require(presaleStarted, "Presale must have started.");
         require(block.timestamp <= ending, "Presale finished.");
-        invested[msg.sender] += msg.value; // update the investors investment
-        require(invested[msg.sender] >= 0.10 ether, "Your investment should be more than 0.10 BNB.");
-        require(invested[msg.sender] <= 10 ether, "Your investment cannot exceed 10 BNB.");
+        invested[msg.sender] += msg.value; // update investors balance
+        require(invested[msg.sender] >= 0.1 ether, "Your investment should be more than 0.1 ETH.");
+        require(invested[msg.sender] <= 2 ether, "Your investment cannot exceed 2 ETH.");
 
-        uint _investorTokens = msg.value * tokensPerETH; // how many PLRT Token will they receive
-        investorBalance[msg.sender] += _investorTokens;
-        withdrawableBalance[msg.sender] += _investorTokens;
+        uint _investorTokens = msg.value * tokensPerETH; // how many tokens they will receive
+        investorBalance[msg.sender] += _investorTokens;//do the swap
+        withdrawableBalance[msg.sender] += _investorTokens;//update the necesary balances
         tokensSold += _investorTokens;
     }
 
-    //% calculator
+    //% calculation
     function mulScale (uint x, uint y, uint128 scale) internal pure returns (uint) {
         uint a = x / scale;
         uint b = x % scale;
@@ -75,41 +99,63 @@ contract SecondPresale is ReentrancyGuard {
 
         return a * c * scale + a * d + b * c + b * d / scale;
     }
-    //claim function for investors
-    function claimTokens() public nonReentrant {
-        require(whitelist[msg.sender], "You must be on the whitelist.");
+    //investors claim function - they claim tokens at the end of the presale 
+
+    //it means a buyer who buys 1000 tokens can take 100 a week every week for x weeks
+    function withdrawTokens() public nonReentrant {
         require(block.timestamp > ending, "Presale must have finished.");
         require(claimReady[msg.sender] <= block.timestamp, "You can't claim now.");
-        uint _contractBalance = token.balanceOf(address(this));
-        require(_contractBalance > 0, "Insufficient contract balance.");
+        require(ContractBalance > 0, "Insufficient contract balance.");
         require(investorBalance[msg.sender] > 0, "Insufficient investor balance.");
 
         uint _withdrawableTokensBalance = mulScale(investorBalance[msg.sender], 1000, 10000); // 1000 basis points = 10%.
 
         if(withdrawableBalance[msg.sender] <= _withdrawableTokensBalance) {
             token.transfer(msg.sender, withdrawableBalance[msg.sender]);
-
             investorBalance[msg.sender] = 0;
             withdrawableBalance[msg.sender] = 0;
         } else {
-            claimReady[msg.sender] = block.timestamp + cooldownTime; // udate next claim time
-
-            withdrawableBalance[msg.sender] -= _withdrawableTokensBalance; // update balance
-
-            token.transfer(msg.sender, _withdrawableTokensBalance); // transfer tokens
+            claimReady[msg.sender] = block.timestamp + cooldownTime; // update next claim time
+            withdrawableBalance[msg.sender] -= _withdrawableTokensBalance; // update withdrawable balance
+            token.transfer(msg.sender, _withdrawableTokensBalance); // transfer the tokens
         }
     }
 
-    function BalanceOut() public onlyOwner {
-        uint _bnbBalance = address(this).balance;
-        payable(owner).transfer(_bnbBalance);
-    }
-    //burn leftover tokens
+    //burn left over tokens
     function burnTokens() public onlyOwner {
-        require(block.timestamp > ending, "Presale must have finished.");
-        
-        uint _contractBalance = token.balanceOf(address(this));
-        uint _tokenBalance = _contractBalance - tokensSold;
-        token.transfer(deadWallet, _tokenBalance);
+        require(block.timestamp > ending, "Presale must have finished.");        
+        uint _burnBalance = ContractBalance - tokensSold;
+        token.transfer(deadWallet, _burnBalance);
+    }
+
+    function BalanceOut() public onlyOwner {
+        uint _Balance = address(this).balance;
+        payable (owner).transfer(_Balance);
+    }
+
+    receive() external payable{
+        balance += msg.value;
+    }
+
+    fallback() external payable{
+        balance += msg.value;
     }
 }
+
+interface IERC20 {
+    function decimals() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function onERC20Received(address _operator, address _from, uint256 _value, bytes calldata _data) external returns(bytes4);
+}
+
+https://etherscan.io/address/0x4e76d9ad7bd91b079d9a691a2c9afc1ef19728b3#code
+
+
+
